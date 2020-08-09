ba528587f1d047d52251119bfca17fa406cdb52c
Luigi Campanaro <luigi@oxfordrobotics.institute>
2020-08-09 11:32:16 +0100

    All the variables are now generated ad doubles

--- spinup/algos/pytorch/ddpg/core.py
+++ spinup/algos/pytorch/ddpg/core.py
@@ -47,7 +47,7 @@ class MLPActor_CPG(nn.Module, CPGControllerHopf):
     def forward(self, obs=None):
         assert obs is not None, "Obs are 'None'."
         d = self.updateControlCommands(obs)
-        v = self.sym.float() @ self.v_short + self.fixed.float()
+        v = self.sym @ self.v_short + self.fixed
 
         if obs is not None:
             sigma_N = self.contactFeedback(obs)
