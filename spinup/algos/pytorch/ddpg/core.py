Minor FIX: drive had wrong dimensions

spinup/algos/pytorch/ddpg/core.py
@@ -42,7 +42,7 @@ class CPGActor(nn.Module, CPGControllerHopf):
         CPGControllerHopf.__init__(self, *args, **kwargs)
     def forward(self, obs=None):
         assert obs is not None, "Obs are 'None'."
-        d = obs[:len(self.drives), 0].double()
+        d = obs[:len(self.drives), 0].double().view(-1, 1)
         v = self.sym @ self.v_short + self.fixed
 
         sigma_N = self.contactFeedback(obs)